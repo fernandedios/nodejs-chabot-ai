ee29c560067b01ffe340f756fe1af00d6f488d97
Fernan de Dios <info@mediagramstudios.com>
2017-12-21 12:48:34 +0800

    added production environment keys

--- config/config.js
+++ config/config.js
@@ -1,3 +1,20 @@
-const config = require('./keys.json'); // json automatically converted to object
-// console.log(config.production);
-module.exports = config.production;
+let env = process.env.NODE_ENV || 'development';
+let config = {};
+let confKeys;
+
+if (env === 'development') {
+  const keys = require('./keys.json'); // json automatically converted to object
+  confKeys = keys["dev"];
+}
+else if (env === 'production') {
+  confKeys = process.env;
+}
+
+Object.keys(confKeys) // gets object keys and returns it as an array
+  .forEach((key) => {
+    config[key] = confKeys[key];
+  });
+
+// console.log(config);
+
+module.exports = config;
